ebe45ed8cd04e1a46f507db287866bcca3af57b5
Samicostox <sami.ribardiere@gmail.com>
2023-12-15 17:25:02 +0100

    toto

--- src/Qodly/table.js
+++ src/Qodly/table.js
@@ -1,6 +1,7 @@
 // CSVTable.js
 import React, { useState } from 'react';
 import Papa from 'papaparse';
+import { CodeBlock, dracula } from 'react-code-blocks';
 
 const CSVTable = () => {
   const [tableData, setTableData] = useState([]);
@@ -20,34 +21,70 @@ const CSVTable = () => {
   };
 
   const getCellStyle = (header, cell) => {
-    let style = {};
+    let style = {
+      maxWidth: '200px', // Default maxWidth
+      whiteSpace: 'normal',
+      wordWrap: 'break-word',
+      overflowWrap: 'break-word',
+    };
 
-    // Apply background color for specific columns
+    if (header === 'Response_3.5' || header === 'Response_4_Vector') {
+      style.maxWidth = '500px';
+    } else if (header.toLowerCase().includes('time')) {
+      style.maxWidth = '150px';
+    }
+
     if (['Response_3.5', 'Response_4_Vector', 'Response_GPT4'].includes(header)) {
       switch (header) {
         case 'Response_3.5':
-          style.backgroundColor = '#faf5ff'; // Light purple
+          style.backgroundColor = '#faf5ff';
           break;
         case 'Response_4_Vector':
-          style.backgroundColor = '#fce7f3'; // Light pink
+          style.backgroundColor = '#fce7f3';
           break;
         case 'Response_GPT4':
-          style.backgroundColor = '#ccfbf1'; // Slightly darker purple
+          style.backgroundColor = '#ccfbf1';
           break;
         default:
           break;
       }
     }
 
-    // Apply text color for numeric values
     const value = parseFloat(cell);
     if (!isNaN(value)) {
-      if (value > 10) style.color = '#ef4444'; // Red for values above 10
-      else if (value > 5) style.color = '#f59e0b'; // Orange for values between 5 and 10
-      else style.color = '#10b981'; // Green for values under 5
+      if (value > 10) style.color = '#ef4444';
+      else if (value > 5) style.color = '#f59e0b';
+      else style.color = '#10b981';
     }
 
     return style;
+  };
+
+  const renderCellContent = (cell, header) => {
+    if (header === 'Response_GPT4') {
+      // Splitting the content by lines and processing each line
+      return cell.split('\n').map((line, index) => {
+        // Check if a line contains the start or end of a code block
+        if (line.startsWith('```') || line.endsWith('```')) {
+          // Extract the code (excluding the backticks)
+          const code = line.replace(/```/g, '').trim();
+          // Render as a code block if it's not an empty string
+          return code && <CodeBlock text={code} language="javascript" theme={dracula} key={index} />;
+        } else {
+          // Render as normal text
+          return <span key={index}>{line}<br/></span>;
+        }
+      });
+    }
+    try {
+      const parsedCell = JSON.parse(cell);
+      if (parsedCell && typeof parsedCell === 'object' && parsedCell.response) {
+        return <pre style={{ whiteSpace: 'pre-wrap' }}>{parsedCell.response}</pre>;
+      }
+    } catch (error) {
+      // Not a JSON string, return as is
+    }
+    return cell;
   };
 
   return (
@@ -70,7 +107,7 @@ const CSVTable = () => {
               <tr className={`${index % 2 === 0 ? 'bg-white' : 'bg-gray-50'} border-b`} key={index}>
                 {Object.entries(row).map(([header, cell], idx) => (
                   <td className="py-4 px-6 border-r border-gray-300" style={getCellStyle(header, cell)} key={idx}>
-                    {cell}
+                    {renderCellContent(cell, header)}
                   </td>
                 ))}
               </tr>
@@ -83,18 +120,3 @@ const CSVTable = () => {
 };
 
 export default CSVTable;
-
-
-
-const getCellBackground = (header) => {
-    switch (header) {
-      case 'Response_3.5':
-        return 'bg-purple-100'; // Light purple
-      case 'Response_4_Vector':
-        return 'bg-pink-100'; // Light pink
-      case 'Response_GPT4':
-        return 'bg-teal-100'; // Slightly darker purple
-      default:
-        return '';
-    }
-  };
